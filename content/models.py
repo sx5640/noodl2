4594b279d1ee4ac56fd0278ff58abeff8a36925f
sx5640 <sx5640@msn.com>
2017-03-23 11:24:41 -0400

    content models added

--- content/models.py
+++ content/models.py
@@ -3,3 +3,24 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Source(models.Model):
+    
+    #attributes
+    title = models.CharField(max_length = 200)
+    url = models.URLField(max_length = 400)
+    rss = models.URLField(max_length = 400)
+    creation_time = models.DateTimeField(auto_now_add = True)
+    last_update = models.DateTimeField()
+    
+class Article(models.Model):
+    
+    #attributes
+    title = models.CharField(max_length = 200)
+    url = models.URLField(max_length = 400)
+    image_url = models.URLField(max_length = 400)
+    author = models.CharField(max_length = 200)
+    description = models.TextField()
+    publication_time = models.DateTimeField(auto_now_add = True)
+    source = models.ForeignKey(Source, on_delete = models.CASCADE)
+    
+        
